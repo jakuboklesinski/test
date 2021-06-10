09281f1cfb87a877eb65a01340a3cd477cf8b305
jakuboklesinski <83898938+jakuboklesinski@users.noreply.github.com>
2021-06-10 19:15:10 +0200

    Update web_interface_tests.py

--- web_interface_tests.py
+++ web_interface_tests.py
@@ -37,7 +37,7 @@ class TestFunctionalDnaasm(unittest.TestCase):
             alert.accept()
         except:
             pass
-##test 2
+##drjghsrkljghrkljh
     def clickCssLink(self, ident, interval=0.1, maxTime=2):
         """Searches for an css and clicks it"""
         browser = self.browser
